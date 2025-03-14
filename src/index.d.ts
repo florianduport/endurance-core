d8427a3a6c43a0ec7e14550c0dcf92877cf37a3a
Florian Duport <florianduport@mac.lan>
2025-03-14 17:43:34 +0100

    TS bug fixes

---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+declare module 'endurance-core' {
+    export { app } from './lib/app.js';
+    export { auth, accessControl } from './lib/auth.js';
+    export { enduranceConsumer } from './lib/consumer.js';
+    export { enduranceCron } from './lib/cron.js';
+    export { enduranceDatabase } from './lib/database.js';
+    export { enduranceEmitter, enduranceEventTypes } from './lib/emitter.js';
+    export { enduranceListener } from './lib/listener.js';
+    export { enduranceNotificationManager } from './lib/notification.js';
+    export { createRouter, Request, Response } from './lib/router.js';
+    export { enduranceSwagger } from './lib/swagger.js';
+}
